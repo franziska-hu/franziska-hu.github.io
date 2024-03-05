2a2a583d97b49f7fe0f694589bd1a554d39fe78c
Franziska Huber <huber@webit.de>
2024-03-05 11:30:32 +0100

    fix custom font and add landscape styling for detail view

--- docs/assets/js/detail-overlay.js
+++ docs/assets/js/detail-overlay.js
@@ -168,29 +168,31 @@ function getOverlay(data) {
                 <ul class="overlay__content-lines-wrapper">
                     ${lineMarkup}
                 </ul>
-                </div>
+            </div>
 
+            <div class="wrapper--horizontal-view">
                 <div class="overlay__content-departures">
                     <div class="overlay_content-depatures-header">
                         <h3>Abfahrten</h3>
                         <div class="live-hint">
-                        Live
-                        <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M4.53225 12.622L3.42075 13.7507C1.3275 12.1 0 9.691 0 7C0 4.309 1.3275 1.9 3.42075 0.25L4.53225 1.378C2.78775 2.75425 1.67925 4.762 1.67925 7C1.67925 9.238 2.78775 11.2458 4.53225 12.622ZM5.0385 7C5.0385 5.65675 5.70375 4.45225 6.75 3.6265L5.63925 2.5C4.24425 3.60025 3.35925 5.206 3.35925 7C3.35925 8.794 4.24425 10.3997 5.63925 11.5L6.75 10.3735C5.703 9.54775 5.0385 8.34325 5.0385 7ZM16.3207 7C16.3207 4.762 15.2123 2.75425 13.4678 1.378L14.5792 0.25C16.6725 1.9 18 4.309 18 7C18 9.691 16.6725 12.1 14.5792 13.7507L13.4678 12.622C15.2123 11.2458 16.3207 9.238 16.3207 7ZM11.25 10.3735L12.3608 11.5C13.7558 10.3997 14.6407 8.794 14.6407 7C14.6407 5.206 13.7558 3.60025 12.3608 2.5L11.25 3.6265C12.297 4.45225 12.9615 5.65675 12.9615 7C12.9615 8.34325 12.297 9.54775 11.25 10.3735ZM9 4.75C7.758 4.75 6.75 5.75725 6.75 7C6.75 8.24275 7.758 9.25 9 9.25C10.242 9.25 11.25 8.24275 11.25 7C11.25 5.75725 10.242 4.75 9 4.75Z" fill="white"/>
-                        </svg>  
+                            Live
+                            <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4.53225 12.622L3.42075 13.7507C1.3275 12.1 0 9.691 0 7C0 4.309 1.3275 1.9 3.42075 0.25L4.53225 1.378C2.78775 2.75425 1.67925 4.762 1.67925 7C1.67925 9.238 2.78775 11.2458 4.53225 12.622ZM5.0385 7C5.0385 5.65675 5.70375 4.45225 6.75 3.6265L5.63925 2.5C4.24425 3.60025 3.35925 5.206 3.35925 7C3.35925 8.794 4.24425 10.3997 5.63925 11.5L6.75 10.3735C5.703 9.54775 5.0385 8.34325 5.0385 7ZM16.3207 7C16.3207 4.762 15.2123 2.75425 13.4678 1.378L14.5792 0.25C16.6725 1.9 18 4.309 18 7C18 9.691 16.6725 12.1 14.5792 13.7507L13.4678 12.622C15.2123 11.2458 16.3207 9.238 16.3207 7ZM11.25 10.3735L12.3608 11.5C13.7558 10.3997 14.6407 8.794 14.6407 7C14.6407 5.206 13.7558 3.60025 12.3608 2.5L11.25 3.6265C12.297 4.45225 12.9615 5.65675 12.9615 7C12.9615 8.34325 12.297 9.54775 11.25 10.3735ZM9 4.75C7.758 4.75 6.75 5.75725 6.75 7C6.75 8.24275 7.758 9.25 9 9.25C10.242 9.25 11.25 8.24275 11.25 7C11.25 5.75725 10.242 4.75 9 4.75Z" fill="white"/>
+                            </svg>  
+                        </div>
                     </div>
+
+                    <ul class="overlay__content-departures-wrapper">
+                        ${departureMarkup}
+                    </ul>
                 </div>
 
-                <ul class="overlay__content-departures-wrapper">
-                    ${departureMarkup}
-                </ul>
-            </div>
-
-            <div class="overlay__content-detour">
-            <h3>Aktuelle Umleitungen</h3>
-            <ul class="overlay__content-detour-wrapper">
-                ${routeChangesMarkup}
-            </ul>
+                <div class="overlay__content-detour">
+                    <h3>Aktuelle Umleitungen</h3>
+                    <ul class="overlay__content-detour-wrapper">
+                        ${routeChangesMarkup}
+                    </ul>
+                </div>
             </div>
         </div>
     `
@@ -284,7 +286,6 @@ function initOverlayForSearch(data) {
 
 window.addEventListener("DOMContentLoaded", (event) => {
     if (!document.querySelector('.stop-information-box')) return;
-    console.log('search trigger');
 
     fetch('./search-detail-response.json')
     .then((response) => response.json())
